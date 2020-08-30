141a655294df671e6170f871b273cb60b3b12d29
Vladimir Gekov <vladimirgekov7@gmail.com>
2020-08-30 14:19:21 +0300

    Add files via upload

---
+++ 01.GITHUB-COMMITS/app.js
@@ -0,0 +1,22 @@
+const usernameEl = document.getElementById("username");
+const repoEl = document.getElementById("repo");
+const commitsUlEl = document.getElementById("commits");
+
+function getUrl(username, repo) {
+  return `https://api.github.com/repos/${username}/${repo}/commits`;
+}
+
+function loadCommits() {
+  const username = usernameEl.value;
+  const repo = repoEl.value;
+  const url = getUrl(username, repo);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = `${item.commit.author.name} ${item.commit.message}`;
+        commitsUlEl.appendChild(li);
+      });
+    });
+}
